e0e3d790ed179600dd56f60e2cbab8ca33d524d7
Xin Dong <xindong9511@gmail.com>
2021-07-07 22:35:23 +0800

    fix test

--- test/config.sh
+++ test/config.sh
@@ -7,8 +7,8 @@ python setup.py install
 
 # SCRIPT setting --show
 
-SCRIPT setting --human_chip_index '/fs/home/dongxin/Projects/SCRIPT/indices/index/human_ChIP_index' --human_motif_index '/fs/home/dongxin/Projects/SCRIPT/indices/index/human_motif_index'
-SCRIPT setting --mouse_chip_index '/fs/home/dongxin/Projects/SCRIPT/indices/index/mouse_ChIP_index' --mouse_motif_index '/fs/home/dongxin/Projects/SCRIPT/indices/index/mouse_motif_index'
+SCRIPT setting --human_chip_index '/fs/home/dongxin/Projects/SCRIPT/indices/human/ChIP_top10k' --human_motif_index '/fs/home/dongxin/Projects/SCRIPT/indices/human/motif_top10k_resize'
+SCRIPT setting --mouse_chip_index '/fs/home/dongxin/Projects/SCRIPT/indices/mouse/ChIP_top10k' --mouse_motif_index '/fs/home/dongxin/Projects/SCRIPT/indices/mouse/motif_top10k_resize'
 SCRIPT setting --show
 
 
--- test/unit_test.sh
+++ test/unit_test.sh
@@ -7,8 +7,8 @@ python setup.py install
 
 # SCRIPT setting --show
 
-SCRIPT setting --human_chip_index '/fs/home/dongxin/Projects/SCRIPT/indices/index/human_ChIP_index' --human_motif_index '/fs/home/dongxin/Projects/SCRIPT/indices/index/human_motif_index'
-SCRIPT setting --mouse_chip_index '/fs/home/dongxin/Projects/SCRIPT/indices/index/mouse_ChIP_index' --mouse_motif_index '/fs/home/dongxin/Projects/SCRIPT/indices/index/mouse_motif_index'
+SCRIPT setting --human_chip_index '/fs/home/dongxin/Projects/SCRIPT/indices/human/ChIP_top10k' --human_motif_index '/fs/home/dongxin/Projects/SCRIPT/indices/human/motif_top10k_resize'
+SCRIPT setting --mouse_chip_index '/fs/home/dongxin/Projects/SCRIPT/indices/mouse/ChIP_top10k' --mouse_motif_index '/fs/home/dongxin/Projects/SCRIPT/indices/mouse/motif_top10k_resize'
 SCRIPT setting --show
 
 SCRIPT enrich -e ../test/sc_experiment.h5ad -i ../test/peak_count.h5 -s hs -p ../test/project --cell_number auto --peak_confidence auto --reference integration -t 8 -y
